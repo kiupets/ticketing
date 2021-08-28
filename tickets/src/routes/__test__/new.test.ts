b36489be300ee2122d54bbb4e7fda2186fef26c3
kiupets <gonzalogg1975@gmail.com>
2021-08-28 19:01:56 -0300

    new tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,26 +1,31 @@
-import request from 'supertest';
-import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
-import { natsWrapper } from '../../nats-wrapper';
+import request from 'supertest'
+import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
+import { natsWrapper } from '../../nats-wrapper'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
-  const response = await request(app).post('/api/tickets').send({});
+  const response = await request(app)
+    .post('/api/tickets')
+    .send({})
 
-  expect(response.status).not.toEqual(404);
-});
+  expect(response.status).not.toEqual(404)
+})
 
 it('can only be accessed if the user is signed in', async () => {
-  await request(app).post('/api/tickets').send({}).expect(401);
-});
+  await request(app)
+    .post('/api/tickets')
+    .send({})
+    .expect(401)
+})
 
 it('returns a status other than 401 if the user is signed in', async () => {
   const response = await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({});
+    .send({})
 
-  expect(response.status).not.toEqual(401);
-});
+  expect(response.status).not.toEqual(401)
+})
 
 it('returns an error if an invalid title is provided', async () => {
   await request(app)
@@ -28,18 +33,18 @@ it('returns an error if an invalid title is provided', async () => {
     .set('Cookie', global.signin())
     .send({
       title: '',
-      price: 10,
+      price: 10
     })
-    .expect(400);
+    .expect(200)
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      price: 10,
+      price: 10
     })
-    .expect(400);
-});
+    .expect(400)
+})
 
 it('returns an error if an invalid price is provided', async () => {
   await request(app)
@@ -47,51 +52,51 @@ it('returns an error if an invalid price is provided', async () => {
     .set('Cookie', global.signin())
     .send({
       title: 'asldkjf',
-      price: -10,
+      price: -10
     })
-    .expect(400);
+    .expect(400)
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'laskdfj',
+      title: 'laskdfj'
     })
-    .expect(400);
-});
+    .expect(400)
+})
 
 it('creates a ticket with valid inputs', async () => {
-  let tickets = await Ticket.find({});
-  expect(tickets.length).toEqual(0);
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
 
-  const title = 'asldkfj';
+  const title = 'asldkfj'
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
       title,
-      price: 20,
+      price: 20
     })
-    .expect(201);
+    .expect(201)
 
-  tickets = await Ticket.find({});
-  expect(tickets.length).toEqual(1);
-  expect(tickets[0].price).toEqual(20);
-  expect(tickets[0].title).toEqual(title);
-});
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].price).toEqual(20)
+  expect(tickets[0].title).toEqual(title)
+})
 
 it('publishes an event', async () => {
-  const title = 'asldkfj';
+  const title = 'asldkfj'
 
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
       title,
-      price: 20,
+      price: 20
     })
-    .expect(201);
+    .expect(201)
 
-  expect(natsWrapper.client.publish).toHaveBeenCalled();
-});
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
